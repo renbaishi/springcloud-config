ec9ac88e9dc2c9515415182200a43f50fa9b8079
renbaishi <47263513+renbaishi@users.noreply.github.com>
2026-02-09 16:47:07 +0800

    Update test comment with new test string

--- test.go
+++ test.go
@@ -5,6 +5,6 @@ import (
 )
 
 func main() {
-  /* // for test: 192.168.9.1:9999*/
+  /* // for test: HYAvjHMK/YgAxmvNYnU025TJMlb95vqwRU2ndN/ftUTIfBLE5qteuiTlaoZhjaBjr4RsnpK2Ow5UQUM/9AFGMsjJdggsBJ5YwEBtVjv3D4ohqRwWR1NG4Bzy23EoeNqQobKL98d3PKIypRxL/aPLJEwhc6eFRlFoLcKPm9gvmJuJWgQA+jGqm4vTSPM8obV4gSLHmDZlES6GPGQ9rKmwMmv2qB04LxUG3mjE3aUAvgxmz4ZvW9yKVE4WoSmp4uQk5GaXcm5eJXLU+pVDE5L3rszY3yQ/tk3/iwgScFksg80/SCej4efMvXx+shm1VwiffnSpyRIZ3ijUbDzfJe/G/A==*/
   fmt.Println("test:", os.Args[1]) 
 }
